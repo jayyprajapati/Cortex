623d85a07758df22ffe4c612e88e3e3ba3fbf603
Jay Prajapati <jay.prajapati5717@gmail.com>
2026-03-22 01:15:16 +0530

    Chore: move data to env file and refer them using dotenv package

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
-EMBEDDING_MODEL = "BAAI/bge-small-en"
+from dotenv import load_dotenv
+import os
 
-VECTOR_SIZE = 384
+load_dotenv()
+
+OLLAMA_CLOUD_API_KEY = os.getenv("OLLAMA_CLOUD_API_KEY")
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "BAAI/bge-small-en")
+VECTOR_SIZE = int(os.getenv("VECTOR_SIZE", 384))
 
 COLLECTION_NAME = "documents"
